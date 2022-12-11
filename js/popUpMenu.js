d83a1d8af46324a38a4fd06212450fce7c6f573a
DaGestapo <105201337+DaGestapo@users.noreply.github.com>
2022-12-11 16:05:57 +0300

    fix more buggs

--- js/popUpMenu.js
+++ js/popUpMenu.js
@@ -3,6 +3,7 @@ import {popUpLink, showLinks} from './features/menuFeatures.js';
 const menu = document.querySelectorAll('.menu');
 const popUpMenu = document.querySelector('.popUpMenu');
 const body = document.querySelector('body');
+const header = document.querySelector('.sticky__header');
 
 let check = true;
 
@@ -17,12 +18,14 @@ export function showPopUpMenu() {
             if(check) {
                 popUpMenu.classList.add('show');
                 body.style.backgroundColor = '#8d8d8d';
-    
+
+                header.style.visibility = 'hidden';
                 check = false;
             } else {
                 popUpMenu.classList.remove('show');
                 body.style.backgroundColor = '#fff';
     
+                header.style.visibility = 'visible';
                 check = true;
             }
         });
